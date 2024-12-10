145d621fb3eea50e6d3dddfcb26b840f1bd25923
Daniel Malede <danielmalede@gmail.com>
2024-12-10 18:00:28 +0200

    last

--- MyString.java
+++ MyString.java
@@ -67,12 +67,12 @@ public class MyString {
     public static String spacedString(String str) {
         //// Replace the following statement with your code
         String word = "";
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length()-1; i++) {
             if (str.length() > 0) {
                 word += (str.charAt(i) +" ");
             }
         }
-        word.substring(word.length()-1,word.length()-1);
+        word+= str.charAt(str.length()-1);
         return word;
     }
 
