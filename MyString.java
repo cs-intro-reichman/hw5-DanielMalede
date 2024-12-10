cd362bc5cda14cf11caf561d9a97cfbd1e51d3d6
Daniel Malede <danielmalede@gmail.com>
2024-12-10 18:36:36 +0200

    length

--- MyString.java
+++ MyString.java
@@ -74,7 +74,7 @@ public class MyString {
         }
         if (word != "") {
             char none = '\u0000';
-            word.replace(word.charAt(word.length()-1),none);
+            word.replace(word.charAt(word.length()),none);
         }
         return word;
     }
