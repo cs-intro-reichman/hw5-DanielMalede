2911fd03cca2f7dcf8c373f0b03fd80e9e5e7f88
Daniel Malede <danielmalede@gmail.com>
2024-12-10 18:35:13 +0200

    hh

--- MyString.java
+++ MyString.java
@@ -118,7 +118,7 @@ public class MyString {
         for (int i = 0; i < str2.length(); i++) {
             for (int j = 0; j < str1.length(); j++) {
                 if (str2.charAt(i) == str1.charAt(j)) {
-                    str1.replace(str2.charAt(i), none);
+                    str1.replace(str1.charAt(i), none);
                 }
             }
         }
